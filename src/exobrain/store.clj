bc3c2d488948f0c9d8e7bc3a1092fac22a454fd3
Gao Chun <sfme@qq.com>
2020-02-14 01:27:01 +0800

    修改运行目录名称.

--- src/exobrain/store.clj
+++ src/exobrain/store.clj
@@ -3,9 +3,11 @@
             [clojure.java.io :as io]
             [taoensso.nippy :as nippy]))
 
-(def rocksdb-dir (.getPath (io/file (System/getProperty "user.home") ".exobrain" "rocksdb")))
+(def rocksdb-dir (.getPath (io/file (System/getProperty "user.home") ".exobrain" "data" "raw")))
 
-(def db (rocks/create-db rocksdb-dir
-                         {:key-encoder nippy/freeze :key-decoder nippy/thaw
-                          :val-encoder nippy/freeze :val-decoder nippy/thaw}))
+;(def db (rocks/create-db rocksdb-dir
+;                         {:key-encoder nippy/freeze :key-decoder nippy/thaw
+;                          :val-encoder nippy/freeze :val-decoder nippy/thaw}))
 
+(defn init
+  [])
